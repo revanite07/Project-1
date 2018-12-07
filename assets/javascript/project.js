84f016ccda6c18c32b68f69ed23a20c8ea7d77de
Calum Ragan <searagan@gmail.com>
2018-12-07 10:10:11 -0800

    trying to deploy

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -4,7 +4,7 @@ var inputDate;
 var database = firebase.database().ref();
 var searchResults = [];
 
-
+// Updates the display to show the new firebase searches
 function updateDisplay(){
   $('#search-counter').empty();
   for(var i = 0;searchResults.length > i;i++){
@@ -198,7 +198,7 @@ function mapCrimeData(data) {
 }
  
 
-
+// Calls firebase data and displays it on the page
 firebase.database().ref('posts').on('child_added', function(childSnapshot){
   if(searchResults.length > 5){
     searchResults.pop()
